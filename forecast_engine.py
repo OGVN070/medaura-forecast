72db484b34c58599cef03e73e583025da4254ae3
OGVN070 <onder.guvenn@gmail.com>
2026-04-26 21:23:37 +0300

    Update forecast_engine.py

--- forecast_engine.py
+++ forecast_engine.py
@@ -5,8 +5,8 @@ from prophet import Prophet
 import plotly.express as px
 
 # --- 1. BAĞLANTI ---
-URL = "https://vbmzsfrbfgbxfbqlrutx.supabase.co/rest/v1/"
-KEY = "sb_publishable_m3NadgtQs-AijCRabSPm6A_UQ0eRfiN"
+URL = "https://vbmzsfrbfgbxfbqlrutx.supabase.co"
+KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdWJhc2UiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5ODUwMTQsImV4cCI6MjAyOTU2MTAxNH0.CIn8v0y1Wf9O9fWp_M0vX_Uv6P8v0v0v0v0v0v0v0v0"
 supabase = create_client(URL, KEY)
 
 st.set_page_config(page_title="MedAura Sales Forecast", layout="wide")
