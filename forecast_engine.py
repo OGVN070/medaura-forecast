ef8bb395c0c2c9322e6b4b31cf3d764967ad25e1
OGVN070 <onder.guvenn@gmail.com>
2026-04-27 12:03:43 +0300

    Update forecast_engine.py

--- forecast_engine.py
+++ forecast_engine.py
@@ -2,12 +2,10 @@ import streamlit as st
 from supabase import create_client
 import pandas as pd
 
-# --- 1. BAĞLANTI ---
-try:
-    # Kasadan bilgileri çekiyoruz
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    supabase = create_client(url, key)
+# Kasadan bilgileri çekiyoruz
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+supabase = create_client(url, key)
 except Exception as e:
     st.error(f"Bağlantı hatası: {e}")
     st.stop()
